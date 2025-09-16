d0f197c08d2c9a4a8bc047c7aac149468c0bb8f6
Aljp <aldairjuradopolo1993@gmail.com{>
2025-09-16 13:41:52 -0500

    Agregar archivos de proyecto.

---
+++ ejer_1/ejer_1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejer_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double v;
+            Console.WriteLine("Ingrese la distancia recorrida (en kilometros): ");
+            double dsaut = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Ingrese el tiempo que tardo en recorrer la distancia (en horas): ");
+            double t = Convert.ToDouble(Console.ReadLine());
+            v = Math.Round(dsaut / t);
+            Console.WriteLine("La velocidad del auto es: " + v + " km/h");
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ ejer_2/ejer_2/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejer_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double angulo;
+            Console.WriteLine("Ingrese un angulo : ");
+            angulo = Convert.ToDouble(Console.ReadLine());
+            if (angulo == 0)
+            {
+                Console.WriteLine("El angulo es nulo");
+            }
+            else if (angulo > 0 && angulo < 90)
+            {
+                Console.WriteLine("El angulo es agudo");
+            }
+            else if (angulo == 90)
+            {
+                Console.WriteLine("El angulo es recto");
+            }
+            else if (angulo > 90 && angulo < 180)
+            {
+                Console.WriteLine("El angulo es obtuso");
+            }
+            else if (angulo == 180)
+            {
+                Console.WriteLine("El angulo es llano");
+            }
+            else if (angulo > 180 && angulo < 360)
+            {
+                Console.WriteLine("El angulo es concavo");
+            }
+            else if (angulo == 360)
+            {
+                Console.WriteLine("El angulo es completo");
+            }
+            else
+            {
+                Console.WriteLine("El valor invalido");
+            }
+            Console.WriteLine("El angulo es: " + angulo);
+            Console.ReadKey();
+
+        }
+    }
+}
